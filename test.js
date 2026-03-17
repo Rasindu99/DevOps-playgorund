0bd7ceac5741757e87cc6b38f452930de306e901
Rasindu99 <rasindu0823@gmail.com>
2026-03-17 18:14:07 +0530

    Implement CI pipeline

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-console.log("Hello, World! Its a beautiful day to code in JavaScript!");
+console.log('Starting tests ....');
+
+console.log('Waiting for 3 seconds to complete the tests ...');
+setTimeout(() => {
+    console.log('Tests completed successfully! 🎉');
+}, 3000);
